6981b253120f926c0b32c6457436a9763f4ceb29
shaiadul <devshaiadul69@gmail.com>
2022-09-10 16:36:01 +0600

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import logo from './Assets/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Bismilla i will start</h1>
+     <h1 className='text-green-400'>Bismilla i will start</h1>
     </div>
   );
 }
