4ade66cabb003d3ecf0c88ecbcd66e931325c2e8
shaiadul <devshaiadul69@gmail.com>
2022-09-11 17:59:14 +0600

    active nav link setup

--- src/pages/Navbar/Navside.js
+++ src/pages/Navbar/Navside.js
@@ -21,9 +21,9 @@ const Navside = () => {
                         <MdDashboardCustomize className='h-5 w-5'></MdDashboardCustomize>
                     </label>
                 } */}
-                <img className='sm:w-52 w-48' src={logo} alt="" />
+                <img className='sm:w-28 w-20' src={logo} alt="" />
                 <span onChange={navBtnHndle} className='md:hidden absolute right-6  top-[22px]'>{toggle ? <MdOutlineClose></MdOutlineClose> : <GoThreeBars></GoThreeBars>}</span>
-                <ul onChange={navBtnHndle} className={`flex flex-col text-center md:justify-end z-10  md:flex-row md:top-0 left-0 w-full md:relative md:opacity-100  absolute  py-4 md:py-0 duration-500 ${toggle ? " opacity-100  top-14" : " top-[-250px] opacity-0"}`}>
+                <ul onChange={navBtnHndle} className={`flex flex-col p-3 font-serif font-bold text-center md:justify-end z-10  md:flex-row md:top-0 left-0 w-full md:relative md:opacity-100  absolute  py-4 md:py-0 duration-500 ${toggle ? " opacity-100  top-14" : " top-[-250px] opacity-0"}`}>
                     <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'navLink')} to={"/"}>Home</NavLink>
                     
                     <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'navLink')} to={"/myPortfolio"}>My Portfolio</NavLink>
